fdcb0487219f3ee26629417afee0105633323552
Dick <honchunkin@gmail.com>
2021-09-11 16:27:38 +0800

    feat: a simple static Task component

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,7 @@
+import "../src/index.css";
+
 export const parameters = {
+  // * create a cb() that appears in the actions panel of the Storybook UI
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
